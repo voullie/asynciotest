31a8c36b315c0db2fc82b9361f1e092bb6c87de6
U-SRT2009\ulas <ulas@ULAS1.srt2009.local>
2017-06-01 09:25:49 +0200

    blabla

--- asyncio_test.py
+++ asyncio_test.py
@@ -4,25 +4,39 @@ import os
 from test_commands import TEST_COMMANDS
 
 os.makedirs('./log', exist_ok=True)
+
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.FileHandler('./log/asynciotest.log'))
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('./log/asynciotest.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter(FORMAT)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 async def handle_echo(reader, writer):
-    global logger
-    data = await reader.read(160)
-    message = data.decode()
+
+    # data = await reader.read(160)
+    # message = data.decode()
+    # addr = writer.get_extra_info('peername')
+    # logm = 'Received "{0}" from {1}'.format(message.strip(), addr)
+    # logger.debug(logm)
+    # 
+    # logger.debug("Sending ({}): {}".format(addr, message.strip()))
+    # writer.write(data)
+    # await writer.drain()
+    #
+    # print("Closing client socket ({})".format(addr))
+    # writer.close()
+
     addr = writer.get_extra_info('peername')
-    logm = 'Received "{0}" from {1}'.format(message.strip(), addr)
-    logger.debug(logm)
+    for cmd in TEST_COMMANDS:
+        message = cmd
+        logger.debug("Sending ({}): {}".format(addr, message.strip()))
+        writer.write(data)
+        await writer.drain()
 
-    logger.debug("Sending ({}): {}".format(addr, message.strip()))
-    writer.write(data)
-    await writer.drain()
-
-    print("Closing client socket ({})".format(addr))
-    writer.close()
 
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, '127.0.0.1', 56700, loop=loop)
